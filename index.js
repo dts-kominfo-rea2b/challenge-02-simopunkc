15e485db2154858899e8e4e39590e0e98982aa59
simopunkc <pembela.allah@gmail.com>
2022-05-28 08:15:17 +0700

    submit challenge 2 oleh budiyanto

--- index.js
+++ index.js
@@ -23,19 +23,19 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
+  let jumlahWanita = 0;
 
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
@@ -48,7 +48,24 @@ function lakukanLooping(arrPegawai) {
       Apabila imbang, maka komentar akan berisi:
         "Jumlah Pria dan Wanita berimbang"
   */
-  let komentar = null;
+  let komentar = "";
+
+  dataYangAkanDilooping.map((v)=>{
+    hasilLooping.push(`${v.namaDepan} ${v.namaBelakang}`)
+    if(v.jenisKelamin=="M"){
+      jumlahPria++
+    }else if(v.jenisKelamin=="F"){
+      jumlahWanita++
+    }
+  })
+
+  if(jumlahPria>jumlahWanita){
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  }else if(jumlahPria<jumlahWanita){
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }else if(jumlahPria==jumlahWanita){
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
